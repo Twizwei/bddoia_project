45f4a5e4a1d692d2b6e9c1831c2f840b638c9fb4
Twizwei <epyiran@outlook.com>
2020-06-30 10:04:45 -0400

    fix train

--- maskrcnn/maskrcnn-benchmark/action_prediction/train.py
+++ maskrcnn/maskrcnn-benchmark/action_prediction/train.py
@@ -101,52 +101,24 @@ def train(cfg, args):
                 # pred, pred_reason = model(imBatch)
                 pred_reason = model(imBatch)
                 # Joint loss
-                # loss1 = criterion(pred, targetBatch)
-                # writer.add_scalar('Train/Loss1', loss1.cpu().data.item(), niter)
-                if args.shrink_loss:
-                    loss2 = shrink_pred(pred_reason, targetBatch, reasonBatch, criterion2)
-                else:
-                    loss2 = criterion2(pred_reason, reasonBatch)
-                writer.add_scalar('Train/Loss2', loss2.cpu().data.item(), niter)
+                loss1 = criterion(pred, targetBatch)
+                loss2 = criterion2(pred_reason, reasonBatch)
 
-                # loss = loss1 + loss2
-                loss = loss2
-                writer.add_scalar('Train/Loss', loss2.cpu().data.item(), niter)
 
-                # loss1 = criterion(pred, targetBatch)
-                # idx = reasonBatch > 0
+                loss = loss1 + loss2
 
             else:
                 pred = model(imBatch)
                 loss = criterion(pred, targetBatch)
 
-            # torch.cuda.empty_cache()
-            # pred, selected_boxes = model(imBatch)
-            # DrawBbox(ori_img_cpu[0], selected_boxes[0])
-            # plt.clf()
-            # plt.close()
-
-            # print(pred)
-            # print(targetBatch)
-
             loss.backward()
             optimizer.step()
             loss_cpu = loss.cpu().data.item()
 
-            
-
             lossArr.append(loss_cpu)
             meanLoss = np.mean(np.array(lossArr))
 
-            # Calculate accuracy
-            # predict = torch.sigmoid(pred) > 0.5
 
-            # f1 = f1_score(target_cpu.data.numpy(), predict.cpu().data.numpy(), average='samples')
-            # AccuracyArr.append(f1)
-            # meanAcc = np.mean(np.array(AccuracyArr))
-
-            # log to TFborad
-            # writer.add_scalar('Train/Acc', f1, niter)
 
             if cfg.MODEL.SIDE:
                 predict_reason = torch.sigmoid(pred_reason) > 0.5
@@ -154,84 +126,26 @@ def train(cfg, args):
                 AccSideArr.append(f1_side)
 
             if i % 50 == 0:
-                
-                # print('prediction logits:', pred)
-                # print('ground truth:', targetBatch.cpu().data.numpy())
                 print('Epoch %d Iteration %d: Loss %.5f Accumulated Loss %.5f' % (
                     epoch, i, loss_cpu, meanLoss))
                 trainingLog.write('Epoch %d Iteration %d: Loss %.5f Accumulated Loss %.5f \n' % (
                     epoch, i, loss_cpu, meanLoss))
-                # print('Epoch %d Iteration %d Action Prediction: F1 %.5f Accumulated F1 %.5f' % (
-                    # epoch, i, AccuracyArr[-1], meanAcc))
                 if cfg.MODEL.SIDE:
                     meanAccSide = np.mean(AccSideArr)
                     print('Epoch %d Iteration %d Side Task: F1 %.5f Accumulated F1 %.5f' % (
                         epoch, i, AccSideArr[-1], meanAccSide))
 
                 print("Selector Output:")
-                # print(torch.flatten(hook_scores.output.data))
                 
         scheduler.step()
 
         if (epoch + 1) % 2 == 0:
             torch.save(model.state_dict(), (outdir + 'net_%d.pth' % (epoch + 1)))
-        # if args.val and epoch % 10 == 0:
-        #    print("Validation...")
-        #    run_test(cfg, args)
+
     print("Saving final model...")
     torch.save(model.state_dict(), (outdir + 'net_Final.pth'))
     writer.export_scalars_to_json(os.path.join(outdir, 'scalars.json'))
     print("Done!")
-
-def shrink_pred(pred_reason, actionTarget, reasonTarget, criterion):
-    loss = torch.tensor([0], dtype=torch.float32).cuda()
-    pred = []
-    target = []
-    if actionTarget.shape[1] == 4:
-        for i in range(actionTarget.shape[0]):
-            if actionTarget[i][0]:
-                pred.append(pred_reason[i][0:3])
-                target.append(reasonTarget[i][0:3])
-            if actionTarget[i][1]:
-                pred.append(pred_reason[i][3:9])
-                target.append(reasonTarget[i][3:9])
-            if actionTarget[i][2]:
-                pred.append(pred_reason[i][9:12])
-                pred.append(pred_reason[i][15:18])
-                target.append(reasonTarget[i][9:12])
-                target.append(reasonTarget[i][15:18])
-            if actionTarget[i][3]:
-                pred.append(pred_reason[i][12:15])
-                pred.append(pred_reason[i][18:21])
-                target.append(reasonTarget[i][12:15])
-                target.append(reasonTarget[i][18:21])
-    elif actionTarget.shape[1] == 6:
-        for i in range(actionTarget.shape[0]):
-            if actionTarget[i][0]:
-                pred.append(pred_reason[i][0:3])
-                target.append(reasonTarget[i][0:3])
-            if actionTarget[i][1]:
-                pred.append(pred_reason[i][3:9])
-                target.append(reasonTarget[i][3:9])
-            if actionTarget[i][2]:
-                pred.append(pred_reason[i][15:18])
-                target.append(reasonTarget[i][15:18])
-            if actionTarget[i][3]:
-                pred.append(pred_reason[i][18:21])
-                target.append(reasonTarget[i][18:21])
-            if actionTarget[i][4]:
-                pred.append(pred_reason[i][9:12])
-                target.append(reasonTarget[i][9:12])
-            if actionTarget[i][5]:
-                pred.append(pred_reason[i][12:15])
-                target.append(reasonTarget[i][12:15])
-
-    if len(pred) == 0:
-        return torch.tensor(0).cuda()
-    pred = torch.cat(pred).cuda()
-    target = torch.cat(target).cuda()
-    loss = criterion(pred, target)
-    return loss
 
 def main():
     # Build a parser for arguments
@@ -313,12 +227,6 @@ def main():
         type=str,
         help="Directory to the trained model",
         default="."
-    )
-    parser.add_argument(
-        "--shrink_loss",
-        action='store_true',
-        default=False,
-        help='whether shrink loss2'
     )
     parser.add_argument(
         "--num_epoch",
